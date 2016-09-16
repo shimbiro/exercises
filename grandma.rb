24461151a328511fd3151844045140f411090ead
shimbiro <shimbirosharon@gmail.com>
2016-09-16 13:10:21 +0300

    grandma talking

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,22 @@
-conversation=gets.chomp
-conversation=="loud"
-puts "#{conversation},ati unasema".upcase
+attempts=0
+until attempts==5
 end
+talk=gets.chomp
+if talk=="BYE"
+	attempts=attempts+1
+else
+	attempts=0
+end
+if talk==talk.upcase
+	#if am loud she says "HIO ILIKUWA 1904"
+	puts "HIO ILIKUWA 1904" unless talk=="BYE"
+else
+	#if am not loud she replies "ATI?? UNASEMA?"
+	puts "ATI?? UNASEMA?"
+end
+puts attempts=0
+puts "BYE" if attempts==5
+end
+	
+
 
